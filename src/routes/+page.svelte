84a7438f8d8249cf3e1c33bf10e6ced273887105
Deric Mendez <deric@crema.us>
2026-02-24 10:26:15 -0600

    Add user authentication handling and dynamic navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<nav>
-    <ul>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/items">Items</a></li>
-    </ul>
-</nav>
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+{#if data.user}
+	<h1>Welcome back, {data.user.name}!</h1>
+	<nav>
+		<ul>
+			<li><a href="/items">My Items</a></li>
+			<li><a href="/logout">Logout</a></li>
+		</ul>
+	</nav>
+{:else}
+	<h1>Welcome to Lending Library</h1>
+	<nav>
+		<ul>
+			<li><a href="/login">Login</a></li>
+		</ul>
+	</nav>
+{/if}
