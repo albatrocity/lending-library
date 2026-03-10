7c8249e8424fbde8711c1ad7b36ec18cbce22db8
Ross Brown <ross@crema.us>
2026-03-10 15:14:24 -0500

    removes beta version migrations, generates 0.4 migrations

--- drizzle/20260303211246_eager_malice/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TYPE "borrowStatus" ADD VALUE IF NOT EXISTS 'pending' BEFORE 'active';--> statement-breakpoint
-ALTER TABLE "borrows" ALTER COLUMN "status" SET DEFAULT 'pending'::"borrowStatus";
